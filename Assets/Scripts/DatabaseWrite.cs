04934828442af7520e29d9bd43a134f69ff54a2c
Vrenato <vargarenato30@gmail.com>
2024-02-19 20:57:42 +0100

    Mysql
    Mysql pluginok telepítése
    Mysql connection létrehozása
    Mysql adatbázisba játékosnév küldése

---
+++ Assets/Scripts/DatabaseWrite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MySql.Data.MySqlClient;
+
+public class DatabaseWrite : MonoBehaviour
+{
+    public Text playername;
+    private string ConnectionString;
+    private MySqlConnection MS_Connection;
+    private MySqlCommand MS_Command;
+    string query;
+
+    public void sendInfo()
+    {
+        connection();
+        query = "insert into jatekos(Nev) values('" + playername.text + "');";
+        MS_Command = new MySqlCommand(query, MS_Connection);
+        MS_Command.ExecuteNonQuery();
+        MS_Connection.Close();
+
+    }
+
+    public void connection()
+    {
+        ConnectionString = "Server = localhost ; Database = zerogravity ; User = root; Password = ; Charset = utf8;";
+        MS_Connection = new MySqlConnection(ConnectionString);
+
+        MS_Connection.Open();
+    }
+
+}
