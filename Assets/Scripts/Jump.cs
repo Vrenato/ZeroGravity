a4ab0b54ca0a985caa86165be0291fa96afcd5a0
Vrenato <vargarenato30@gmail.com>
2024-02-26 20:55:41 +0100

    Imported character

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    public Rigidbody rb;
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 500);
+        }
+    }
+}
