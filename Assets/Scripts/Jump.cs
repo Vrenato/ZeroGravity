68c7e8133111b8d46cc319183e0333ce445a0587
Vrenato <vargarenato30@gmail.com>
2024-02-26 22:23:22 +0100

    More Character import

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -6,12 +6,31 @@ public class Jump : MonoBehaviour
 {
 
     public Rigidbody rb;
-   
+    public float MoveSpeed;
+    float Xinput;
+    float Yinput;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
+        Xinput = Input.GetAxis("Horizontal");
+        Yinput = Input.GetAxis("Vertical");
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector3.up * 500);
+            rb.AddForce(Vector3.up * 5);
         }
     }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Xinput * MoveSpeed, 0, Yinput * MoveSpeed);
+    }
+
 }
