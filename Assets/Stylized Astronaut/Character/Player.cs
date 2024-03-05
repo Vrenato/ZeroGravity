5879cec9cf6383c11cc917d5aac633ec2a45de10
Vrenato <vargarenato30@gmail.com>
2024-03-05 22:26:45 +0100

    Visuals4

--- Assets/Stylized Astronaut/Character/Player.cs
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -10,26 +10,53 @@ public class Player : MonoBehaviour {
 		public float turnSpeed = 400.0f;
 		private Vector3 moveDirection = Vector3.zero;
 		public float gravity = 20.0f;
+		public float jumpHeight;
+		
 
-		void Start () {
+
+	void Start () {
 			controller = GetComponent <CharacterController>();
 			anim = gameObject.GetComponentInChildren<Animator>();
 		}
 
 		void Update (){
-			if (Input.GetKey ("w")) {
-				anim.SetInteger ("AnimationPar", 1);
-			}  else {
-				anim.SetInteger ("AnimationPar", 0);
-			}
 
-			if(controller.isGrounded){
+		if (Input.GetKey ("w"))
+		{
+			anim.SetInteger ("AnimationPar", 1);
+		}  
+
+		else
+		{
+			anim.SetInteger("AnimationPar", 0);
+		}
+
+	
+
+
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			
+			moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+			//animacio hozzarendelese az ugrashoz
+			//anim.SetInteger("AnimationPar", 1);
+		}
+
+		
+
+
+		if (controller.isGrounded){
 				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+			
 			}
 
 			float turn = Input.GetAxis("Horizontal");
 			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
-		}
+
+
+			
+	}
 }
